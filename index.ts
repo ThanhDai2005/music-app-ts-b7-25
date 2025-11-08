b83b43de7c83c0fb32779a5004672603119ce804
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-11-08 17:02:02 +0700

    3.1. Tạo routes

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { connect } from "./config/database";
-import Topic from "./models/topics.model";
+import clientRoutes from "./routes/client/index.route";
 
 connect();
 
@@ -12,15 +12,7 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
