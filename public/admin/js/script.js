57d9f9fadda20096bebe0ddfdfcd92622894a4c5
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-11-25 22:43:46 +0700

    1.2. Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,22 @@ if (uploadImage) {
   });
 }
 // End Upload Image
+
+// Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+  const source = uploadAudio.querySelector("source");
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      source.src = URL.createObjectURL(file);
+
+      uploadAudioPlay.load();
+    }
+  });
+}
+// End Upload Audio
